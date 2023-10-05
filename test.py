fbac94999ac79dc989cb0830a1c49949b8433d4a
dr1nkz <raptorpower1@gmail.com>
2023-10-06 00:20:39 +0300

    Static places

--- test.py
+++ test.py
@@ -58,17 +58,14 @@ def gen(address):
     bboxes = []
     for line in lines:
         line = line.replace('\n', '')
-        line = line.split()
+        line = line.split(', ')
         bbox = [float(l) for l in line]
         bboxes.append(bbox)
     parked_drums_boxes = np.array(bboxes)
 
     # Сколько кадров подряд с пустым местом мы уже видели
     free_space_frames = 0
-    # Пустые места
-    free_space_boxes = None
 
-    first_entry = True
     while cap.isOpened():
 
         # Кадр с камеры
@@ -79,73 +76,59 @@ def gen(address):
 
         # Детектирование
         yolov8_detector(frame)
-        detected_img = yolov8_detector.draw_detections(frame)
+        detected_img = frame
+        # detected_img = yolov8_detector.draw_detections(frame)
         drums_boxes = yolov8_detector.get_boxes()
 
-        if parked_drums_boxes.shape[0] == 0:
-            parked_drums_boxes = np.append(
-                parked_drums_boxes, drums_boxes, axis=0)
-        else:
-            # Если новая катушка >
-            # Если катушка только попала в кадр =
-            if len(drums_boxes) >= len(parked_drums_boxes):
-                parked_drums_boxes = np.append(
-                    parked_drums_boxes, drums_boxes, axis=0)
-                print(parked_drums_boxes)
+        # чтобы не ломалось iou
+        if drums_boxes.shape[0] == 1:
+            # drums_boxes = np.array([drums_boxes])
+            drums_boxes = np.array(drums_boxes).reshape(1, -1)
 
-            all_spaces_free = False
+        # list с координатами пустых мест
+        free_space_boxes = []
+        # free_space = len(parked_drums_boxes) * [False]
 
-            # Если ни одна катушка не найдена, то только нарисовать пустые места
-            if (drums_boxes is None) or (len(drums_boxes) == 0):
-                if free_space_boxes is None:
-                    continue
-                else:
-                    all_spaces_free = True
-                    # free_space_boxes = parked_drums_boxes
+        for i in range(len(parked_drums_boxes)):
+            IoUs = compute_iou(parked_drums_boxes[i], drums_boxes)
+            max_IoU = np.max(IoUs)
+            if max_IoU < 0.15:
+                # Отмечаем, что мы нашли как минимум оно свободное место.
+                # free_space[i] = True
+                free_space_boxes.append(
+                    parked_drums_boxes[i].astype('int'))
 
-            if not (all_spaces_free):
-                # Если только 1 катушка, то чтобы не ломалось IoU
-                if drums_boxes.shape[0] == 1:
-                    # drums_boxes = np.array([drums_boxes])
-                    drums_boxes = np.array(drums_boxes).reshape(1, -1)
+        # Штриховые рамки вокруг всех парковочных мест.
+        for parked_drums_box in parked_drums_boxes:
+            x1, y1, x2, y2 = parked_drums_box.astype('int')
+            # print(parked_drums_box)
+            # Штриховая рамка
 
-                # list с координатами пустых мест
-                free_space_boxes = []
-                # free_space = len(parked_drums_boxes) * [False]
+            cv2.rectangle(detected_img, (x1, y1), (x2, y2),
+                          (127, 127, 127), thickness=2, lineType=cv2.LINE_8)
 
-                for i in range(len(parked_drums_boxes)):
-                    IoUs = compute_iou(parked_drums_boxes[i], drums_boxes)
-                    max_IoU = np.max(IoUs)
-                    if max_IoU < 0.15:
-                        # Отмечаем, что мы нашли как минимум оно свободное место.
-                        # free_space[i] = True
-                        free_space_boxes.append(
-                            parked_drums_boxes[i].astype('int'))
+        # Зелёные рамки вокруг пустых мест.
+        for free_space_box in free_space_boxes:
+            x1, y1, x2, y2 = free_space_box
 
-            # Зелёные рамки вокруг пустых мест.
-            for free_space_box in free_space_boxes:
-                x1, y1, x2, y2 = free_space_box
+            # Зелёная рамка
+            cv2.rectangle(detected_img, (x1, y1), (x2, y2),
+                          (0, 255, 0), thickness=2)
 
-                # Зелёная рамка
-                cv2.rectangle(detected_img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+            # Отображаем надпись Empty place
+            # font
+            font = cv2.FONT_HERSHEY_SIMPLEX
 
-                # Отображаем надпись Empty place
-                # font
-                font = cv2.FONT_HERSHEY_SIMPLEX
+            # fontScale
+            fontScale = 1
 
-                # fontScale
-                fontScale = 1
+            # Line thickness of 2 px
+            thickness = 1
 
-                # Line thickness of 2 px
-                thickness = 2
+            font = cv2.FONT_HERSHEY_DUPLEX
+            cv2.putText(detected_img, f"Empty place", (x1, y1 - 4 * thickness),
+                        font, fontScale, (0, 255, 0), thickness, cv2.LINE_AA)
 
-                font = cv2.FONT_HERSHEY_DUPLEX
-                cv2.putText(detected_img, f"Empty place", (x1, y1 - 4 * thickness),
-                            font, fontScale, (0, 255, 0), thickness, cv2.LINE_AA)
-
-        if first_entry is True:
-            first_entry = False
-        # img = onnx_detect_image('./image.jpg')
         if detected_img is None:
             continue
         rgb_image = Image.fromarray(cv2.cvtColor(
@@ -154,10 +137,6 @@ def gen(address):
         rgb_image.save(buf, 'JPEG')
         frame = buf.getbuffer()
         yield b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n--frame\r\n'
-
-    with open(txt, 'w') as f:
-        for parked_drums_box in parked_drums_boxes:
-            f.write(str(parked_drums_box) + '\n')
 
 
 @app.route('/video_feed/')
